665fa706d386405fede0448b5540da7933bfede7
augustolambare <augustolambare@gmail.com>
2025-05-20 21:46:26 -0300

    Add testimonials section with Swiper integration and styling

--- responsive-coffee-website/script.js
+++ responsive-coffee-website/script.js
@@ -12,3 +12,36 @@ menuCloseButton.addEventListener("click",  () => {
     // Close the mobile menu when the close button is clicked
     document.body.classList.toggle("show-mobile-menu");
 });
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    768: {
+        slidesPerView: 2,
+    },
+    1024: {
+        slidesPerView: 3,
+    },
+    }
+});
